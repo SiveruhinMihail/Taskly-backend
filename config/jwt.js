af6dc790f8f5af611c44598b1d50f554f81fd770
SiveruhinMihail <89450.miha@gmail.com>
2025-06-20 17:49:17 +0400

    delete comments

--- config/jwt.js
+++ config/jwt.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
+require('dotenv').config()
 
 module.exports = {
   access: {
     secret: process.env.JWT_ACCESS_SECRET,
-    expiresIn: "1h", // Короткая жизнь
+    expiresIn: '1h',
   },
   refresh: {
     secret: process.env.JWT_REFRESH_SECRET,
-    expiresIn: "30d", // Долгая жизнь
+    expiresIn: '30d',
   },
-};
+}
