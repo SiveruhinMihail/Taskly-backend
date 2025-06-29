e1ce9a5b1c5c50225a20d623d036a32fe5561204
SiveruhinMihail <89450.miha@gmail.com>
2025-06-29 23:55:24 +0400

    Add db

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected!");
+    await mongoose.connect(process.env.MONGODB_URI)
+    console.log('MongoDB подключён')
   } catch (err) {
-    console.error("MongoDB connection error:", err);
-    process.exit(1);
+    console.error('Ошибка подключения к MongoDB:', err)
+    process.exit(1)
   }
-};
-module.exports = connectDB;
+}
+
+module.exports = connectDB
