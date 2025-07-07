66fda5348196abdbd0205e9e53ab3bcfdd02704d
SiveruhinMihail <89450.miha@gmail.com>
2025-07-07 10:50:35 +0400

    Edit .env

--- config/db.js
+++ config/db.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI)
+    await mongoose.connect(
+      `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@mongodb:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}?authSource=admin`,
+    )
     console.log('MongoDB connected')
   } catch (err) {
     console.error('MongoDB connection error:', err)
