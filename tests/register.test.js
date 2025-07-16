fa0038ab69f5dbc028d085d5eb204ad3a0234cbb
SiveruhinMihail <89450.miha@gmail.com>
2025-07-17 03:42:53 +0400

    edit register test

--- tests/register.test.js
+++ tests/register.test.js
@@ -1,3 +1,29 @@
 test('Always true test', () => {
-  expect(true).toBe(true) // Всегда true
+  expect(true).toBe(true)
 })
+
+// пример теста 
+
+/*
+const axios = require('axios')
+const { test, expect } = require('@jest/globals')
+
+
+test('Register user', async () => {
+  const response = await axios.post(
+    `http://localhost:3000/api/auth/register`,
+    {
+      name: 'Weah MeahoBe',
+      email: 'userrrrr@example.com',
+      password: 'Password123!',
+      use: '33dasdsa33',
+    },
+    {
+      headers: { 'Content-Type': 'application/json' },
+    },
+  )
+
+  expect(response.status).toBe(200)
+})
+
+*/
