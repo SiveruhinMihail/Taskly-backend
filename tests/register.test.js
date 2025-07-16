a17d69dd1fe08ca9af6f75fc58311fc368516211
SiveruhinMihail <89450.miha@gmail.com>
2025-07-17 03:07:09 +0400

    change 201 to 200 port

--- tests/register.test.js
+++ tests/register.test.js
@@ -1,12 +1,10 @@
-require('dotenv').config()
 const axios = require('axios')
 const { test, expect } = require('@jest/globals')
 
-const PORT = process.env.PORT
 
 test('Register user', async () => {
   const response = await axios.post(
-    `http://localhost:${PORT}/api/auth/register`,
+    `http://localhost:3000/api/auth/register`,
     {
       name: 'Weah MeahoBe',
       email: 'userrrrr@example.com',
