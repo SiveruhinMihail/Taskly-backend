80427476c77f6b9dfaa29a3b027745176981fe93
SiveruhinMihail <89450.miha@gmail.com>
2025-07-17 04:07:25 +0400

    work with tests system

--- tests/register.test.js
+++ tests/register.test.js
@@ -1,13 +1,5 @@
-test('Always true test', () => {
-  expect(true).toBe(true)
-})
-
-// пример теста 
-
-/*
 const axios = require('axios')
 const { test, expect } = require('@jest/globals')
-
 
 test('Register user', async () => {
   const response = await axios.post(
@@ -25,5 +17,3 @@ test('Register user', async () => {
 
   expect(response.status).toBe(200)
 })
-
-*/
