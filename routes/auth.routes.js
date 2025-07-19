0f2eca593ccc04df3a9e50a5ad2e1f97298f8699
blessing <130278891+stepanchev@users.noreply.github.com>
2025-07-19 22:02:22 +0400

    add get_user

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { refresh, register, login, logout } = require('../controllers/auth.controller')
+const { refresh, register, login, logout, get_user } = require('../controllers/auth.controller')
 const router = express.Router()
 
 router.post('/register', register)
 router.post('/login', login)
 router.post('/refresh', refresh)
 router.post('/logout', logout)
+router.get('/get_user', get_user)
 
 module.exports = router
