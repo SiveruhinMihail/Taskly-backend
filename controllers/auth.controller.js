c7c5568bb1cb05f6c92e1b0ab804f172614f8386
SiveruhinMihail <89450.miha@gmail.com>
2025-07-22 23:31:04 +0400

    eslint check

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,4 @@
 const AuthService = require('../services/auth.service')
-const jwt = require('jsonwebtoken')
 
 // Регистрация
 exports.register = async (req, res) => {
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,4 @@
 const jwt = require('jsonwebtoken')
-const { secret } = require('../config/jwt')
 
 module.exports = (req, res, next) => {
   const authHeader = req.headers.authorization
