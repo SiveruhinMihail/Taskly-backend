22fd2ab2033daa329f1fc7dd5122456c0e32ea7d
blessing <130278891+stepanchev@users.noreply.github.com>
2025-07-28 23:59:13 +0400

    edit controller login, reg

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -30,8 +30,7 @@ exports.login = async (req, res) => {
     })
   } catch (error) {
     res.status(401).json({
-      success: false,
-      message: error.message,
+      data: { detail: error, message: error },
     })
   }
 }
